e63c03cb2d4dfe6c521a26baf3d26b0c348f03ff
Danny Avery <danny.w.avery@gmail.com>
2018-02-07 11:54:15 -0700

    Add TasksController + update web routes

--- routes/web.php
+++ routes/web.php
@@ -11,18 +11,5 @@
 |
 */
 
-Route::get('/tasks', function () {
-    // $tasks = DB::table('tasks')->latest()->get(); // query builder
-    $tasks = \App\Task::all(); // Eleoquent w/ Model Class
-    return view('tasks.index', compact('tasks'));
-});
-
-Route::get('/tasks/{task}', function ($id) {
-    // $task = DB::table('tasks')->find($id); // query builder
-    $task = \App\Task::find($id); // Eloquent
-    return view('tasks.show', compact('task'));
-});
-
-Route::get('about', function() {
-	return view('about');
-});
+Route::get('/tasks', 'TasksController@index');
+Route::get('/tasks/{task}', 'TasksController@show');

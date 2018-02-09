922f3f62bf5a55c055a27322800cedf0726dac6b
Danny Avery <danny.w.avery@gmail.com>
2018-02-08 20:58:50 -0700

    Create view composer for global archives in AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -13,7 +13,15 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        // View::composer() or
+        // with a view composer you can hook into a view anytime a view is loaded
+        // almost like a callback
+        // when 'layouts.sidebar' is loaded, register/run this callback
+        // which binds the archives => App\Post::archives() to the view
+        // hook into 'layouts.sidebar', pass in the view & bind 'archives' to the $view
+        view()->composer('layouts.sidebar', function($view) {
+			$view->with('archives', \App\Post::archives());
+        });
     }
 
     /**
